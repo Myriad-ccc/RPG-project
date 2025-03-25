1acbaa49f452371fca8a75119640cadd9a27878c
Myriad-ccc <off7cal@gmail.com>
2025-03-25 23:01:36 +0200

    Add files via upload

---
+++ Engine/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace Engine
+{
+    public class Player : LivingCreature
+    {
+        public int Gold { get; set; }
+        public int Experience { get; set; }
+        public int Level { get; set; }
+        public List<InventoryItem> Inventory { get; set; }
+        public List<PlayerQuest> Quests { get; set; }
+
+        public Player(int gold, int experience, int level, int currentHitPoints, int maxHitPoints) : base(currentHitPoints, maxHitPoints)
+        {
+            Gold = gold;
+            Experience = experience;
+            Level = level;
+            Inventory = new List<InventoryItem>();
+            Quests = new List<PlayerQuest>();
+        }
+    }
+}
---
+++ UltraAdventure/UltraAdventure.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Forms;
+using Engine;
+
+namespace UltraAdventure
+{
+    public partial class UltraAdventure : Form
+    {
+        public UltraAdventure()
+        {
+            InitializeComponent();
+
+            Player _player = new Player(100, 100, 50, 0, 1);
+
+            Location location = new Location(1, "Home", "This is your house");
+
+            lblHitPoints.Text = _player.CurrentHitPoints.ToString();
+            lblGold.Text = _player.Gold.ToString();
+            lblExperience.Text = _player.Experience.ToString();
+            lblLevel.Text = _player.Level.ToString();
+        }
+    }
+}
